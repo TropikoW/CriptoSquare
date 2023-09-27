2fc085f47a17a46651e147ae036e1d1b67724cae
Jovanny <jovannypersonal@gmail.com>
2023-09-27 16:21:07 -0500

    page already 40%

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 const containerImagesClothes = document.querySelector('#container_images_main');
 const containerModalBuy = document.querySelector('#container_modal_buy');
+const containerModalWsGM = document.querySelector('#container_modal_ws_gm');
 
 let updatedPrice = '26/09/23 - 17:36 PM';
 
@@ -36,15 +37,34 @@ let tshirtoversizewhite = new Clothes(2,'Shirts','Camiseta Oversize Blanca','../
 
 function loadPage() {
     function sendOrder() {
+        function sendOrderWsGmail() {
+            containerModal = `
+                <div class="container_modal_wsGmail">
+                    <section class="container_contain_wsGmail">
+                        <figure>
+                            <img src="../resources/img/whatsapp-logo-png.png" class="btn_ws_gmail">
+                        </figure>
+                    </section>
+                </div>
+            `
+            containerModalWsGM.innerHTML = containerModal;
+        };
         containerModal = `
-            <div class="container_modal_buyload">
+            <div class="container_modal_buyload" id="container_modal_buyload">
                 <section class="container_contain_modal_buyload">
+                    <figure>
+                        <img src="../resources/img/icone-x-avec-cercle-gris.png" class="icon_close_formpayload" id="icon_close_formpayload">
+                    </figure>
                     <h2>¡Gracias por tu compra!</h2>
                     <label>En el momento no contamos con un portal de pagos online, por ello las ordenes se procesaran por redes alternativas como Whatsapp o Gmail para que puedas incluir la información como dirección y el metodo de pago que tengas disponible para el momento de la entrega, al chat o al correo también podras agregar las dudas o detalles adicionales que desees.</label>
                     <figure>
                         <img src="../resources/img/money-transfer-a7.svg" class="img_contain_sect_buyorder">
                     </figure>
-                    <form>
+                    <h2>Revisión de compra</h2>
+                    <ul>
+                        <li></li>
+                    </ul>
+                    <form class="form_modal_payload" id="form_modal_payload">
                         <label>Dirección</label>
                         <input type="text" placeholder="Indica tu dirección acá" autocomplete="street-address">
                         <label>Punto de referencia</label>
@@ -55,15 +75,29 @@ function loadPage() {
                         <input type="number" placeholder="Indica tu número de celular acá" autocomplete="tel">
                         <label>Indica algunas caracteristicas especiales de tu pedido</label>
                         <textarea></textarea>
+                        <input type="submit" value="Enviar" class="inputsubmit_payload" id="inputsubmit_payload"></input>
                     </form>
-                    <h2>Revisión de compra</h2>
-                    <ul>
-                        <li></li>
-                    </ul>
                 </section>
             </div>
         `;
         containerModalBuy.innerHTML = containerModal;
+        const containerModalBuyLoad = document.querySelector('#container_modal_buyload');
+        const iconCloseFormPaylaod = document.querySelector('#icon_close_formpayload');
+        const inputSubmitPayload = document.querySelector('#inputsubmit_payload');
+        inputSubmitPayload.addEventListener('click',hideFirstModal);
+        iconCloseFormPaylaod.addEventListener('click',hideFirstModal);
+        function showFirstModal() {
+            containerModalBuyLoad.classList.add('container_modal_buyload_show');
+        };
+        function hideFirstModal() {
+            containerModalBuyLoad.classList.remove('container_modal_buyload_show');
+        }
+        showFirstModal();
+        const formModalPayload = document.querySelector('#form_modal_payload');
+        formModalPayload.addEventListener('submit',function(event){
+            event.preventDefault();
+            sendOrderWsGmail();
+        });
     };
    
     tshirt.push(tshirtoversizeblack,tshirtoversizewhite);
