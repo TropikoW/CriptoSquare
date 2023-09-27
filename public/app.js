f97e8ed7ca6c56110f3cb9396058fe238e236618
Jovanny <jovannypersonal@gmail.com>
2023-09-27 17:30:49 -0500

    already the photos

--- public/app.js
+++ public/app.js
@@ -31,8 +31,8 @@ function convertNumbertoMoney(a) {
     return b;
 };
 
-let tshirtoversizeblack = new Clothes(1,'Shirts','Camiseta Oversize Negra','../resources/img/oversize_unicolor_negra_2.png',['Blanco','Negro'],40000,0.00038,'Camiseta Unicolor cuello redondo, el tamaño de manga es 3/4,el tipo de tela es liso hecho de poly algodón.',['M','L'],generateLimitArray(3));
-let tshirtoversizewhite = new Clothes(2,'Shirts','Camiseta Oversize Blanca','../resources/img/oversize_unicolor_blanca_2.png',['Blanco','Negro'],40000,0.00038,'Camiseta Unicolor cuello redondo, el tamaño de manga es 3/4,el tipo de tela es liso hecho de poly algodón.',['S','M'],generateLimitArray(3));
+let tshirtoversizeblack = new Clothes(1,'Shirts','Camiseta Oversize Negra','./resources/img/oversize_unicolor_negra_2.png',['Blanco','Negro'],40000,0.00038,'Camiseta Unicolor cuello redondo, el tamaño de manga es 3/4,el tipo de tela es liso hecho de poly algodón.',['M','L'],generateLimitArray(3));
+let tshirtoversizewhite = new Clothes(2,'Shirts','Camiseta Oversize Blanca','./resources/img/oversize_unicolor_blanca_2.png',['Blanco','Negro'],40000,0.00038,'Camiseta Unicolor cuello redondo, el tamaño de manga es 3/4,el tipo de tela es liso hecho de poly algodón.',['S','M'],generateLimitArray(3));
 
 
 function loadPage() {
@@ -42,7 +42,7 @@ function loadPage() {
                 <div class="container_modal_wsGmail">
                     <section class="container_contain_wsGmail">
                         <figure>
-                            <img src="../resources/img/whatsapp-logo-png.png" class="btn_ws_gmail">
+                            <img src="./resources/img/whatsapp-logo-png.png" class="btn_ws_gmail">
                         </figure>
                     </section>
                 </div>
@@ -53,12 +53,12 @@ function loadPage() {
             <div class="container_modal_buyload" id="container_modal_buyload">
                 <section class="container_contain_modal_buyload">
                     <figure>
-                        <img src="../resources/img/icone-x-avec-cercle-gris.png" class="icon_close_formpayload" id="icon_close_formpayload">
+                        <img src="./resources/img/icone-x-avec-cercle-gris.png" class="icon_close_formpayload" id="icon_close_formpayload">
                     </figure>
                     <h2>¡Gracias por tu compra!</h2>
                     <label>En el momento no contamos con un portal de pagos online, por ello las ordenes se procesaran por redes alternativas como Whatsapp o Gmail para que puedas incluir la información como dirección y el metodo de pago que tengas disponible para el momento de la entrega, al chat o al correo también podras agregar las dudas o detalles adicionales que desees.</label>
                     <figure>
-                        <img src="../resources/img/money-transfer-a7.svg" class="img_contain_sect_buyorder">
+                        <img src="./resources/img/money-transfer-a7.svg" class="img_contain_sect_buyorder">
                     </figure>
                     <h2>Revisión de compra</h2>
                     <ul>
